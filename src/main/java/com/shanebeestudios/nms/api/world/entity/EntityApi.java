e8eeaedbf7dd395ee5254194bbed58366ba5dc21
ShaneBee <shanebolenback@me.com>
2023-05-15 11:09:16 -0700

    EntityApi - cleanup imports

--- src/main/java/com/shanebeestudios/nms/api/world/entity/EntityApi.java
+++ src/main/java/com/shanebeestudios/nms/api/world/entity/EntityApi.java
@@ -1,21 +1,7 @@
 package com.shanebeestudios.nms.api.world.entity;
 
-import com.mojang.authlib.GameProfile;
-import com.shanebeestudios.nms.api.reflection.ReflectionShortcuts;
 import com.shanebeestudios.nms.api.reflection.ReflectionUtils;
-import com.shanebeestudios.nms.api.world.WorldApi;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Api methods pertaining to an {@link org.bukkit.entity.Entity}
@@ -23,7 +9,8 @@ import java.util.Map;
 @SuppressWarnings({"deprecation", "unused"})
 public class EntityApi {
 
-    /** Get an NMS Entity from a {@link org.bukkit.entity.Entity Bukkit Entity}
+    /**
+     * Get an NMS Entity from a {@link org.bukkit.entity.Entity Bukkit Entity}
      *
      * @param bukkitEntity Bukkit Entity
      * @return NMS Entity
