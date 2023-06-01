b4e1c85b799f3455ec70a2db25d1cc3fabe4a3b6
ShaneBee <shanebolenback@me.com>
2023-06-01 13:27:03 -0700

    create javadocs on gh-pages

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.shanebeestudios.nms.api.util"},{"l":"com.shanebeestudios.nms.api.world"},{"l":"com.shanebeestudios.nms.api.world.entity"}];updateSearchResults();
