1a6bf05b499ddafd009fde7346c30ab6fbf8d50c
ShaneBee <shanebolenback@me.com>
2023-06-06 15:42:22 -0700

    FakePlayer - add update method for one player

--- src/main/java/com/shanebeestudios/nms/api/world/entity/FakePlayer.java
+++ src/main/java/com/shanebeestudios/nms/api/world/entity/FakePlayer.java
@@ -1,5 +1,6 @@
 package com.shanebeestudios.nms.api.world.entity;
 
+import com.shanebeestudios.nms.api.util.McUtils;
 import net.minecraft.network.protocol.game.ClientboundAddPlayerPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
 import net.minecraft.server.MinecraftServer;
@@ -7,17 +8,21 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.phys.Vec3;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 /**
  * Represents a fake {@link ServerPlayer}
+ * <p>
+ *     Can create in {@link PlayerApi#spawnFakePlayer(String, Location)}
+ * </p>
  */
 @SuppressWarnings({"deprecation", "unused"})
 public class FakePlayer {
 
-    private final ServerPlayer serverPlayer;
+    private final ServerPlayer fakeServerPlayer;
 
     FakePlayer(ServerPlayer serverPlayer) {
-        this.serverPlayer = serverPlayer;
+        this.fakeServerPlayer = serverPlayer;
     }
 
     /**
@@ -27,17 +32,32 @@ public class FakePlayer {
      * @param location Location to teleport to
      */
     public void teleport(Location location) {
-        this.serverPlayer.absMoveTo(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+        this.fakeServerPlayer.absMoveTo(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
         update();
     }
 
-    void update() {
-        for (ServerPlayer serverPlayer : MinecraftServer.getServer().getPlayerList().players) {
-            ServerGamePacketListenerImpl connection = serverPlayer.connection;
-            connection.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER, this.serverPlayer));
-            connection.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.UPDATE_LISTED, this.serverPlayer));
-            connection.send(new ClientboundAddPlayerPacket(this.serverPlayer));
-        }
+    private void update(ServerPlayer serverPlayer) {
+        ServerGamePacketListenerImpl connection = serverPlayer.connection;
+        connection.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER, this.fakeServerPlayer));
+        connection.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.UPDATE_LISTED, this.fakeServerPlayer));
+        connection.send(new ClientboundAddPlayerPacket(this.fakeServerPlayer));
+    }
+
+    /**
+     * Update for a single Player
+     *
+     * @param player Player to update for
+     */
+    public void update(Player player) {
+        ServerPlayer serverPlayer = McUtils.getServerPlayer(player);
+        update(serverPlayer);
+    }
+
+    /**
+     * Update for all players online
+     */
+    public void update() {
+        MinecraftServer.getServer().getPlayerList().players.forEach(this::update);
     }
 
     private Vec3 vecFromLocation(Location location) {
@@ -46,7 +66,7 @@ public class FakePlayer {
 
     @Override
     public String toString() {
-        return "FakePlayer{name='" + this.serverPlayer.getGameProfile().getName() + "'}";
+        return "FakePlayer{name='" + this.fakeServerPlayer.getGameProfile().getName() + "'}";
     }
 
 }
