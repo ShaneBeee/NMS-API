a717f07f92be3af010c6ee609abe48ef9a964707
ShaneBee <shanebolenback@me.com>
2023-08-09 12:15:42 -0700

    ItemApi - add back with deprecation notice

---
+++ src/main/java/com/shanebeestudios/nms/api/world/ItemApi.java
@@ -0,0 +1,12 @@
+package com.shanebeestudios.nms.api.world;
+
+/**
+ * Api methods pertaining to an {@link org.bukkit.inventory.ItemStack}
+ * @deprecated Use {@link com.shanebeestudios.nms.api.world.item.ItemApi} instead of this class
+ */
+@Deprecated // on Aug 9/2023
+public class ItemApi extends com.shanebeestudios.nms.api.world.item.ItemApi {
+
+    private ItemApi() {
+    }
+}
