1f5d1b4c13118b546bc16a566b3e79994d82b21b
ShaneBee <shanebolenback@me.com>
2023-08-31 16:03:15 -0700

    BlockApi - move to block package

--- src/main/java/com/shanebeestudios/nms/api/world/ItemApi.java
+++ src/main/java/com/shanebeestudios/nms/api/world/ItemApi.java
@@ -2,9 +2,10 @@ package com.shanebeestudios.nms.api.world;
 
 /**
  * Api methods pertaining to an {@link org.bukkit.inventory.ItemStack}
+ *
  * @deprecated Use {@link com.shanebeestudios.nms.api.world.item.ItemApi} instead of this class
  */
-@Deprecated // on Aug 9/2023
+@Deprecated(forRemoval = true, since = "1.4.0")
 public class ItemApi extends com.shanebeestudios.nms.api.world.item.ItemApi {
 
     private ItemApi() {
---
+++ src/main/java/com/shanebeestudios/nms/api/world/block/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Api relating to a Minecraft Block
+ */
+package com.shanebeestudios.nms.api.world.block;
