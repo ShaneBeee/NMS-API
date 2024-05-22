64b4a6b2c74554c62921eae0b14c6720cc342c77
ShaneBee <shanebolenback@me.com>
2024-05-21 22:54:51 -0700

    McItem - remove deprecated methods

--- src/main/java/com/shanebeestudios/nms/api/world/item/McItem.java
+++ src/main/java/com/shanebeestudios/nms/api/world/item/McItem.java
@@ -1,12 +1,8 @@
 package com.shanebeestudios.nms.api.world.item;
 
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TieredItem;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.components.FoodComponent;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -53,77 +49,6 @@ public class McItem {
     }
 
     /**
-     * Check if this item is edible
-     *
-     * @return whether this item is edible
-     * @deprecated Can be done in Bukkit now {@link ItemMeta#hasFood()}
-     */
-    @Deprecated(forRemoval = true, since = "May 16/2024")
-    public boolean isEdible() {
-        return this.item.components().has(DataComponents.FOOD);
-    }
-
-    /**
-     * Check if this item can always be eaten
-     *
-     * @return True if can always eat else false
-     * @deprecated Can be done in Bukkit now {@link FoodComponent#canAlwaysEat()}
-     */
-    @Deprecated(forRemoval = true, since = "May 16/2024")
-    public boolean canAlwaysEat() {
-        FoodProperties foodProperties = this.item.components().get(DataComponents.FOOD);
-        return foodProperties != null && foodProperties.canAlwaysEat();
-    }
-
-    /**
-     * Get the nutrition if this is a food item
-     *
-     * @return Nutrition if food item else 0
-     * @deprecated Can be done in Bukkit now, {@link FoodComponent#getNutrition()}
-     */
-    @Deprecated(forRemoval = true, since = "May 16/2024")
-    public int getNutrition() {
-        FoodProperties foodProperties = this.item.components().get(DataComponents.FOOD);
-        return foodProperties != null ? foodProperties.nutrition() : 0;
-    }
-
-    /**
-     * Get the saturation modifier if this is a food item
-     *
-     * @return Saturation modifier if food item else 0
-     * @deprecated Can be done in Bukkit now, {@link FoodComponent#getSaturation()}
-     */
-    @Deprecated(forRemoval = true, since = "May 16/2024")
-    public float getSaturationModifier() {
-        FoodProperties foodProperties = this.item.components().get(DataComponents.FOOD);
-        return foodProperties != null ? foodProperties.saturation() : 0;
-    }
-
-    /**
-     * Check if this item is a meat product
-     *
-     * @return True if meat product else false
-     * @deprecated No longer used in Minecraft
-     */
-    @Deprecated(forRemoval = true, since = "April 27/2024")
-    public boolean isMeat() {
-        return false;
-    }
-
-    /**
-     * Check if this item can be instantly eaten
-     *
-     * @return True if can instantly eat else false
-     * @deprecated No longer used in Minecraft
-     */
-    @Deprecated(forRemoval = true, since = "April 27/2024")
-    public boolean isFastFood() {
-        FoodProperties foodProperties = this.item.components().get(DataComponents.FOOD);
-        if (foodProperties == null) return false;
-        return foodProperties.eatSeconds() <= 0.8f;
-    }
-
-    /**
      * Check if this item is fire-resistant
      *
      * @return True if fire-resistant else false
@@ -139,62 +64,6 @@ public class McItem {
      */
     public boolean isComplex() {
         return this.item.isComplex();
-    }
-
-    /**
-     * Get the damage amount of this item
-     * <p>Only works on Sword and Digger (axe, hoe, pickaxe, shovel) items</p>
-     *
-     * @return Damage amount if applicable else 0
-     * @deprecated No longer used
-     */
-    @Deprecated(forRemoval = true, since = "April 27/2024")
-    public float getDamage() {
-        return 0;
-    }
-
-    /**
-     * Get the Rarity of an Item
-     * <p>Rarity returns different if the ItemStack is enchanted.
-     * If you leave the ItemStack null, it may not give you the correct result.</p>
-     *
-     * @param itemStack ItemStack to check against (enchantments matter) (can be null)
-     * @return Rarity of item
-     * @deprecated Can be done in Bukkit now {@link ItemMeta#getRarity()}
-     */
-    @Nullable
-    @Deprecated(forRemoval = true, since = "May 16/2024")
-    public Rarity getRarity(@Nullable ItemStack itemStack) {
-        net.minecraft.world.item.Rarity rarity = this.item.components().get(DataComponents.RARITY);
-        if (rarity == null) return null;
-        return switch (rarity) {
-            case COMMON -> Rarity.COMMON;
-            case UNCOMMON -> Rarity.UNCOMMON;
-            case RARE -> Rarity.RARE;
-            case EPIC -> Rarity.EPIC;
-        };
-    }
-
-    /**
-     * Item Rarity
-     */
-    public enum Rarity {
-        /**
-         * Common
-         */
-        COMMON,
-        /**
-         * Uncommon
-         */
-        UNCOMMON,
-        /**
-         * Rare
-         */
-        RARE,
-        /**
-         * Epic
-         */
-        EPIC
     }
 
 }
